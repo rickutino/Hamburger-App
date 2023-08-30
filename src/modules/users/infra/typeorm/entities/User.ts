3f7ec7ee5e4bbe71126bbb252bb96f18ad47e9da
rickutino <rickutino@gmail.com>
2023-08-30 20:37:12 +0900

    feat: add User entities from typeorm

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  PrimaryColumn,
+} from 'typeorm'
+import { v4 as uuidV4 } from 'uuid'
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  avatar: string
+
+  @Column()
+  isAdmin: boolean
+
+  @Column()
+  staff: 'kicthen' | 'waitress' | undefined
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
+
+export { User }
