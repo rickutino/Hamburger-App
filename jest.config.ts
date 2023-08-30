311996df9afcf6d869ecf3f36f2de9990f7ee269
rickutino <rickutino@gmail.com>
2023-08-30 16:35:20 +0900

    fix import

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
-import { pathsToModuleNameMapper } from 'ts-jest/utils'
+import { pathsToModuleNameMapper } from 'ts-jest'
 
 import { compilerOptions } from './tsconfig.json'
 
