7406591be902be493b5a100d5b13f27749732bb1
rickutino <rickutino@gmail.com>
2023-09-01 12:49:06 +0900

    test: ensure not return if validation success

--- src/utils/helpers/validators/compareFieldsValidation.spec.ts
+++ src/utils/helpers/validators/compareFieldsValidation.spec.ts
@@ -13,4 +13,11 @@ describe('RequiredField Validation', () => {
 
     expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
+
+  test('Should not return if validation success', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+
+    expect(error).toBeFalsy()
+  })
 })
